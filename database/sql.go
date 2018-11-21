c0b588c0455d4316a6181c03724fd2635bd6bd4c
xiayesuifeng <gdigrgg@gmail.com>
2018-11-22 01:52:51 +0800

    add database package

---
+++ database/sql.go
@@ -0,0 +1,62 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"gitlab.com/xiayesuifeng/gopanel/core"
+	"log"
+	"sync"
+)
+
+var db *gorm.DB
+var once sync.Once
+
+func initDB() {
+	conf := core.Conf.Db
+	args := ""
+
+	switch conf.Driver {
+	case "mysql":
+		args = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=UTC",
+			conf.Username, conf.Password, conf.Address, conf.Port, conf.Dbname)
+	case "postgres":
+		args = fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s",
+			conf.Address, conf.Port, conf.Dbname, conf.Username, conf.Password)
+	}
+
+	var err error
+	db, err = gorm.Open(conf.Driver, args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func Instance() *gorm.DB {
+	once.Do(initDB)
+	return db
+}
+
+func CreateDB() error {
+	conf := core.Conf.Db
+	args := ""
+
+	switch conf.Driver {
+	case "mysql":
+		args = fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8&parseTime=True&loc=UTC",
+			conf.Username, conf.Password, conf.Address, conf.Port)
+	case "postgres":
+		args = fmt.Sprintf("host=%s port=%s user=%s password=%s",
+			conf.Address, conf.Port, conf.Username, conf.Password)
+	}
+
+	db, err := sql.Open(conf.Driver, args)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + conf.Dbname)
+	return err
+}
