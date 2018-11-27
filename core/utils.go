dd26635ebf901239ce513ec19cfdf85236638b45
xiayesuifeng <gdigrgg@gmail.com>
2018-11-28 00:53:05 +0800

    add encryption password function

---
+++ core/utils.go
@@ -0,0 +1,13 @@
+package core
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+)
+
+func EncryptionPassword(password string) string {
+	md5Data := md5.Sum([]byte(password))
+	sha1Data := sha1.Sum([]byte(md5Data[:]))
+	return hex.EncodeToString(sha1Data[:])
+}
