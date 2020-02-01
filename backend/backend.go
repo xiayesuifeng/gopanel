005301654d54ffa17eb5b9d0702c8f64bc47b86b
xiayesuifeng <gdigrgg@gmail.com>
2020-02-02 01:48:02 +0800

    send change notify when log or status have change

--- backend/backend.go
+++ backend/backend.go
@@ -12,25 +12,45 @@ type Backend struct {
 	Status string
 	Cmd    *exec.Cmd
 	Log    bytes.Buffer
+	Notify chan bool
 }
 
 func StartNewBackend(name, path string, arg ...string) {
 	cmd := exec.Command(path, arg...)
 	cmd.Dir = filepath.Dir(path)
 	b := &Backend{Cmd: cmd}
+	b.Notify = make(chan bool, 1)
 
-	cmd.Stdout = &b.Log
-	cmd.Stderr = &b.Log
+	cmd.Stderr = cmd.Stdout
+
+	stdout, _ := cmd.StdoutPipe()
 
 	backendList[name] = b
 
 	go func() {
-		b.Status = "Run"
+		b.SetStatus("Run")
 		cmd.Run()
-		b.Status = "Stop"
+		b.SetStatus("Stop")
+	}()
+
+	go func() {
+		tmp := make([]byte, 1024)
+		for {
+			if b.Status != "Run" {
+				continue
+			}
+			n, _ := stdout.Read(tmp)
+			b.Log.Write(tmp[:n])
+			b.Notify <- true
+		}
 	}()
 }
 
 func GetBackend(name string) *Backend {
 	return backendList[name]
+}
+
+func (b *Backend) SetStatus(status string) {
+	b.Status = status
+	b.Notify <- true
 }
