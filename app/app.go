2b444314d69af97565a06e6e8d742095ddcd1f1f
xiayesuifeng <gdigrgg@gmail.com>
2020-02-13 21:53:33 +0800

    add AddApp function

---
+++ app/app.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"encoding/json"
+	"errors"
+	"gitlab.com/xiayesuifeng/gopanel/backend"
+	"gitlab.com/xiayesuifeng/gopanel/caddy"
+	"strings"
+)
+
+const (
+	GO_TYPE = iota + 1
+	JAVA_TYPE
+	PHP_TYPE
+)
+
+type App struct {
+	Name         string          `json:"name" binding:"required"`
+	CaddyConfig  json.RawMessage `json:"caddyConfig" binding:"required"`
+	Type         int             `json:"type" binding:"required"`
+	Path         string          `json:"path"`
+	BootArgument string          `json:"bootArgument"`
+}
+
+func AddApp(app App) error {
+	if CheckAppExist(app.Name) {
+		return errors.New("app is exist")
+	}
+
+	if err := SaveAppConfig(app); err != nil {
+		return err
+	}
+
+	if err := caddy.AddServer(app.Name, app.CaddyConfig); err != nil {
+		return err
+	}
+
+	if app.Type == GO_TYPE {
+		backend.StartNewBackend(app.Name, app.Path, strings.Split(app.BootArgument, " ")...)
+	}
+
+	return nil
+}
