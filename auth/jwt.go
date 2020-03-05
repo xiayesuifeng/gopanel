e8b67696c5299fcdf23add405d46a7feb2646faa
xiayesuifeng <gdigrgg@gmail.com>
2020-03-05 16:15:47 +0800

    add ParseToken function

--- auth/jwt.go
+++ auth/jwt.go
@@ -12,3 +12,17 @@ func GenerateToken() (string, error) {
 
 	return tokenClaims.SignedString([]byte("gopanel-secret"))
 }
+
+func ParseToken(token string) (claims *jwt.StandardClaims, err error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("gopanel-secret"), nil
+	})
+
+	if tokenClaims != nil {
+		if c, ok := tokenClaims.Claims.(*jwt.StandardClaims); ok {
+			claims = c
+		}
+	}
+
+	return claims, err
+}
