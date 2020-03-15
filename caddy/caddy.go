3d3d506d47dbcfe4fc734db522d60d5640bd47a0
xiayesuifeng <gdigrgg@gmail.com>
2020-03-15 15:36:31 +0800

    add netdata caddy config when netdata enable

--- caddy/caddy.go
+++ caddy/caddy.go
@@ -15,7 +15,7 @@ const panelCaddyJson = `{
           {
             "handler": "subroute",
             "routes": [
-              {
+              {netdataJson}{
                 "handle": [
 				  {
 				    "handler": "reverse_proxy",
@@ -31,6 +31,43 @@ const panelCaddyJson = `{
     ]
   }`
 
+const netdataPathCaddyJson = `{
+	"handler": "rewrite",
+    "uri_substring": [{
+	  "find": "/",
+	  "replace": "{path}",
+	  "limit": 1
+	}]
+  },`
+
+const netdataCaddyJson = `{
+      "handle": [{
+        "handler": "static_response",
+        "headers": {"Location": ["/netdata/"]},
+        "status_code": 302
+      }],
+      "match": [{"path": ["/netdata"]}]
+	},
+	{
+      "handle": [
+	    {
+	      "handler": "rewrite",
+	      "strip_path_prefix": "/netdata"
+	    },
+	    {netdataPathJson}
+        {
+	      "handler": "reverse_proxy",
+	      "upstreams": [{"dial": "{host}"}],
+	      "transport": {
+	        "protocol": "http",
+	    	"tls": {}
+	      },
+          "headers": {"request": {"set": {"Host": ["{http.reverse_proxy.upstream.hostport}"]}}}
+	    }
+	  ],
+	  "match": [{"path": ["/netdata/*"]}]
+	},`
+
 func LoadPanelConfig(port string) (err error) {
 	conf := panelCaddyJson
 	conf = strings.ReplaceAll(conf, "{port}", port)
@@ -39,6 +76,23 @@ func LoadPanelConfig(port string) (err error) {
 		conf = strings.ReplaceAll(conf, "{listenPort}", "\"listen\": [\":"+strconv.Itoa(core.Conf.Panel.Port)+"\"],")
 	} else {
 		conf = strings.ReplaceAll(conf, "{listenPort}", "")
+	}
+
+	if core.Conf.Netdata.Enable {
+		netdataConf := netdataCaddyJson
+		netdataConf = strings.ReplaceAll(netdataConf, "{host}", core.Conf.Netdata.Host)
+		netdataPath := core.Conf.Netdata.Path
+		if netdataPath != "" && netdataPath != "/" {
+			if !strings.HasSuffix(netdataPath, "/") {
+				netdataPath += "/"
+			}
+			pathConf := netdataPathCaddyJson
+			pathConf = strings.ReplaceAll(pathConf, "{path}", netdataPath)
+			netdataConf = strings.ReplaceAll(netdataConf, "{netdataPathJson}", pathConf)
+		}
+		conf = strings.ReplaceAll(conf, "{netdataJson}", netdataConf)
+	} else {
+		conf = strings.ReplaceAll(conf, "{netdataJson}", "")
 	}
 
 	if CheckServerExist("gopanel") {
