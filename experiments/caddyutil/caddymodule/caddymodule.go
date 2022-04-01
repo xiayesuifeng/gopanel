f3b8389736cdae27e79b09e8cee052b5158cc173
xiayesuifeng <gdigrgg@gmail.com>
2022-04-01 17:40:09 +0800

    [caddy module] add HasNonStandardModule and HasPackage func

--- experiments/caddyutil/caddymodule/caddymodule.go
+++ experiments/caddyutil/caddymodule/caddymodule.go
@@ -50,3 +50,34 @@ func GetModuleList() (*ModuleList, error) {
 
 	return list, nil
 }
+
+func (m *ModuleList) HasNonStandardModule(module string) bool {
+	for _, list := range m.NonStandard {
+		for _, name := range list {
+			if name == module {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (m *ModuleList) HasPackage(pkg string) bool {
+	_, ok := m.Standard[pkg]
+	if ok {
+		return true
+	}
+
+	_, ok = m.NonStandard[pkg]
+	if ok {
+		return true
+	}
+
+	_, ok = m.Unknown[pkg]
+	if ok {
+		return true
+	}
+
+	return false
+}
--- experiments/caddyutil/caddymodule/caddymodule_test.go
+++ experiments/caddyutil/caddymodule/caddymodule_test.go
@@ -10,3 +10,29 @@ func TestGetModuleList(t *testing.T) {
 
 	t.Log(list)
 }
+
+func TestModuleList_HasNonStandardModule(t *testing.T) {
+	list, err := GetModuleList()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if list.HasNonStandardModule("dns.providers.cloudflare") {
+		t.Log("dns.providers.cloudflare found")
+	} else {
+		t.Log("dns.providers.cloudflare not found")
+	}
+}
+
+func TestModuleList_HasPackage(t *testing.T) {
+	list, err := GetModuleList()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if list.HasPackage("github.com/caddy-dns/cloudflare") {
+		t.Log("github.com/caddy-dns/cloudflare found")
+	} else {
+		t.Log("github.com/caddy-dns/cloudflare not found")
+	}
+}
