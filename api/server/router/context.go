a0e3be51fd3e5126bba060022572daa3c6df6f9a
xiayesuifeng <gdigrgg@gmail.com>
2023-02-06 12:46:58 +0800

    feat(router): add JSON function for router.Context

--- api/server/router/context.go
+++ api/server/router/context.go
@@ -2,8 +2,15 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Context struct {
 	*gin.Context
+}
+
+func (ctx *Context) JSON(data interface{}) error {
+	ctx.Context.JSON(http.StatusOK, data)
+
+	return nil
 }
