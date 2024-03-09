da1690a24de162ff076f9149e9b839102e5aba4f
xiayesuifeng <gdigrgg@gmail.com>
2024-03-09 15:41:12 +0800

    feat(containify): add containify new and get engine function

--- containify/containify.go
+++ containify/containify.go
@@ -1,1 +1,34 @@
 package containify
+
+import (
+	"errors"
+	"gitlab.com/xiayesuifeng/gopanel/containify/engine"
+)
+
+type Containify struct {
+	engine engine.Engine
+}
+
+func (c *Containify) ContainerEngine() engine.Engine {
+	return c.engine
+}
+
+func New() (*Containify, error) {
+	if !IsEnabled() {
+		return nil, errors.New("containify is not enabled")
+	}
+
+	engineName, setting := GetContainerEngine()
+	if engineName == "" {
+		return nil, errors.New("container engine is not set")
+	}
+
+	e, err := engine.New(engineName, setting)
+	if err != nil {
+		return nil, err
+	}
+
+	instance := &Containify{engine: e}
+
+	return instance, nil
+}
