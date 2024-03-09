ac057a8a3f2130d0f0ce9b7dc03b12d003cf9ee6
xiayesuifeng <gdigrgg@gmail.com>
2024-03-09 15:39:29 +0800

    feat(containify): add container engine

---
+++ containify/engine/engine.go
@@ -0,0 +1,35 @@
+package engine
+
+import (
+	"errors"
+	"log"
+)
+
+var (
+	containerEngines = make(map[string]Engine)
+)
+
+type Engine interface {
+	New(setting []byte) error
+}
+
+func New(engine string, setting []byte) (Engine, error) {
+	containerEngine := containerEngines[engine]
+	if containerEngine == nil {
+		return nil, errors.New("container engine " + engine + " not found")
+	}
+
+	if err := containerEngine.New(setting); err != nil {
+		return nil, err
+	}
+
+	return containerEngine, nil
+}
+
+func Register(name string, engine Engine) {
+	if containerEngines[name] != nil {
+		log.Panicln("containify: container engine " + name + " already registered")
+	}
+
+	containerEngines[name] = engine
+}
