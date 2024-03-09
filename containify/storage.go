6b79438afe4d5c45879058b10766bede45263805
xiayesuifeng <gdigrgg@gmail.com>
2024-03-09 15:31:26 +0800

    feat(containify): add containify setting storage

---
+++ containify/storage.go
@@ -0,0 +1,31 @@
+package containify
+
+import (
+	"gitlab.com/xiayesuifeng/gopanel/core/settingStorage"
+)
+
+const (
+	module                    = "containify"
+	enabledKey                = "enabled"
+	containerEngineKey        = "containerEngine"
+	containerEngineSettingKey = "containerEngineSetting"
+)
+
+func IsEnabled() bool {
+	return string(settingStorage.GetStorage().Get(module, enabledKey, []byte("false"))) == "true"
+}
+
+func SetEnabled(enabled bool) error {
+	if enabled {
+		return settingStorage.GetStorage().Set(module, enabledKey, []byte("true"))
+	} else {
+		return settingStorage.GetStorage().Set(module, enabledKey, []byte("false"))
+	}
+}
+
+func GetContainerEngine() (engine string, setting []byte) {
+	engine = string(settingStorage.GetStorage().Get(module, containerEngineKey, []byte("")))
+
+	setting = settingStorage.GetStorage().Get(module, containerEngineSettingKey, []byte("{}"))
+	return
+}
