a357dd201b84922fe9dc94200bf5fae94c2afdc9
xiayesuifeng <gdigrgg@gmail.com>
2024-03-14 17:44:25 +0800

    feat(api/server): add get containify configuration api to containify endpoint

--- api/server/endpoint/containify/containify.go
+++ api/server/endpoint/containify/containify.go
@@ -1,6 +1,8 @@
 package containify
 
 import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"gitlab.com/xiayesuifeng/gopanel/api/server/router"
 	"gitlab.com/xiayesuifeng/gopanel/containify"
 	"net/http"
@@ -15,7 +17,19 @@ func (c *Containify) Name() string {
 }
 
 func (c *Containify) Run(r router.Router) {
+	r.GET("", c.Get)
+
 	r.Use(c.middleware)
+}
+
+func (c *Containify) Get(ctx *router.Context) error {
+	engine, setting := containify.GetContainerEngine()
+
+	return ctx.JSON(gin.H{
+		"enabled":                containify.IsEnabled(),
+		"containerEngine":        engine,
+		"containerEngineSetting": json.RawMessage(setting),
+	})
 }
 
 func (c *Containify) middleware(ctx *router.Context) error {
