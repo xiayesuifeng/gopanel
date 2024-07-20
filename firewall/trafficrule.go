80591a910b176a7cf02f0f72dce92a0767f79c4e
xiayesuifeng <gdigrgg@gmail.com>
2024-07-21 02:10:18 +0800

    fix(firewall): add TrafficRule type

---
+++ firewall/trafficrule.go
@@ -0,0 +1,55 @@
+package firewall
+
+import (
+	"encoding/json"
+	"net"
+)
+
+type RuleStrategy int
+
+const (
+	AcceptRuleStrategy RuleStrategy = iota
+	RejectRuleStrategy
+	DropRuleStrategy
+)
+
+type RuleType string
+
+const (
+	ServiceRuleType     RuleType = "service"
+	PortRuleType        RuleType = "port"
+	ProtocolRuleType    RuleType = "protocol"
+	ForwardPortRuleType RuleType = "forward-port"
+	SourcePortRuleType  RuleType = "source-port"
+)
+
+type TrafficRule struct {
+	// Family ipv4 or ipv6, empty means both
+	Family string `json:"family"`
+	// SrcAddr source address
+	SrcAddr       net.IPAddr `json:"srcAddr"`
+	SrcAddrInvert bool       `json:"srcAddrInvert"`
+	// DestAddr destination address
+	DestAddr       net.IPAddr      `json:"destAddr"`
+	DestAddrInvert bool            `json:"destAddrInvert"`
+	Strategy       RuleStrategy    `json:"strategy"`
+	Type           RuleType        `json:"type"`
+	Value          json.RawMessage `json:"value"`
+	Log            RuleLog         `json:"log"`
+	Audit          bool            `json:"audit"`
+}
+
+type RuleLog struct {
+	Enabled bool   `json:"enabled"`
+	Prefix  string `json:"prefix"`
+	// Level emerg、alert、crit、error、warning、notice、info or debug
+	Level string `json:"level"`
+	Limit string `json:"limit"`
+}
+
+type Port struct {
+	// Port number or range (8080-8085)
+	Port string
+	// Protocol tcp or udp
+	Protocol string
+}
