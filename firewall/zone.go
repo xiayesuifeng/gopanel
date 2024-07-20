402d787922121e4308d160b36a986d3719172cb1
xiayesuifeng <gdigrgg@gmail.com>
2024-07-20 21:10:11 +0800

    feat(firewall): add get firewalld all zone function

--- firewall/zone.go
+++ firewall/zone.go
@@ -1,5 +1,11 @@
 package firewall
 
+import (
+	"errors"
+	"gitlab.com/xiayesuifeng/go-firewalld"
+	"sync"
+)
+
 type ZoneStrategy string
 
 const (
@@ -36,19 +42,7 @@ func GetZone(name string, permanent bool) (*Zone, error) {
 		return nil, err
 	}
 
-	return &Zone{
-		Name:               zone.Name,
-		Description:        zone.Description,
-		Target:             ZoneStrategy(zone.Target),
-		IngressPriority:    zone.IngressPriority,
-		EgressPriority:     zone.EgressPriority,
-		ICMPBlocks:         zone.ICMPBlocks,
-		ICMPBlockInversion: zone.ICMPBlockInversion,
-		Masquerade:         zone.Masquerade,
-		Forward:            zone.Forward,
-		Interfaces:         zone.Interfaces,
-		Protocols:          zone.Protocols,
-	}, nil
+	return toZone(zone), nil
 }
 
 // UpdateZone update zone setting, target and description field only change in permanent
@@ -86,4 +80,72 @@ func GetZoneNames(permanent bool) ([]string, error) {
 	defer conn.Close()
 
 	return conn.GetZoneNames()
+}
+
+func GetZones(permanent bool) (result []*Zone, err error) {
+	conn, err := getConn(permanent)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	names, err := conn.GetZoneNames()
+	if err != nil {
+		return nil, err
+	}
+
+	var errs []error
+
+	wg := sync.WaitGroup{}
+	var mutex sync.Mutex
+	for _, name := range names {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			zone, err := conn.GetZoneByName(name)
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			if err != nil {
+				errs = append(errs, err)
+				return
+			}
+
+			result = append(result, toZone(zone))
+		}()
+	}
+
+	wg.Wait()
+	err = errors.Join(errs...)
+	return
+}
+
+func toZone(zone *firewalld.Zone) *Zone {
+	icmpBlocks := make([]string, 0)
+	if zone.ICMPBlocks != nil {
+		icmpBlocks = zone.ICMPBlocks
+	}
+	interfaces := make([]string, 0)
+	if zone.Interfaces != nil {
+		interfaces = zone.Interfaces
+	}
+	protocols := make([]string, 0)
+	if zone.Protocols != nil {
+		protocols = zone.Protocols
+	}
+
+	return &Zone{
+		Name:               zone.Name,
+		Description:        zone.Description,
+		Target:             ZoneStrategy(zone.Target),
+		IngressPriority:    zone.IngressPriority,
+		EgressPriority:     zone.EgressPriority,
+		ICMPBlocks:         icmpBlocks,
+		ICMPBlockInversion: zone.ICMPBlockInversion,
+		Masquerade:         zone.Masquerade,
+		Forward:            zone.Forward,
+		Interfaces:         interfaces,
+		Protocols:          protocols,
+	}
 }
