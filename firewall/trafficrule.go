286e8921923ed5b97f247ca17109a71427df95f2
xiayesuifeng <gdigrgg@gmail.com>
2024-07-21 15:34:12 +0800

    feat(firewall): add get TrafficRule list function

--- firewall/trafficrule.go
+++ firewall/trafficrule.go
@@ -2,6 +2,7 @@ package firewall
 
 import (
 	"encoding/json"
+	"strings"
 )
 
 type RuleStrategy int
@@ -53,4 +54,205 @@ type Port struct {
 	Port string
 	// Protocol tcp or udp
 	Protocol string
+}
+
+var ruleElement = []string{
+	"destination",
+	"service",
+	"port",
+	"protocol",
+	"masquerade",
+	"icmp-block",
+	"forward-port",
+	"source-port",
+	"accept",
+	"drop",
+	"reject",
+}
+
+func GetTrafficRules(zone string, permanent bool) ([]*TrafficRule, error) {
+	conn, err := getConn(permanent)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := conn.GetZoneByName(zone)
+	if err != nil {
+		return nil, err
+	}
+
+	rules := make([]*TrafficRule, 0)
+
+	for _, service := range result.Services {
+		bytes, err := json.Marshal(&service)
+		if err != nil {
+			return nil, err
+		}
+
+		rules = append(rules, &TrafficRule{
+			Type:     ServiceRuleType,
+			Value:    bytes,
+			Strategy: AcceptRuleStrategy,
+		})
+	}
+
+	for _, port := range result.Ports {
+		bytes, err := json.Marshal(&Port{
+			port.Port,
+			port.Protocol,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		rules = append(rules, &TrafficRule{
+			Type:     PortRuleType,
+			Value:    bytes,
+			Strategy: AcceptRuleStrategy,
+		})
+	}
+
+	for _, port := range result.SourcePorts {
+		bytes, err := json.Marshal(&Port{
+			port.Port,
+			port.Protocol,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		rules = append(rules, &TrafficRule{
+			Type:     SourcePortRuleType,
+			Value:    bytes,
+			Strategy: AcceptRuleStrategy,
+		})
+	}
+
+	for _, richRule := range result.RichRules {
+		rule := &TrafficRule{}
+
+		args := strings.Split(richRule, " ")
+		for i := 1; i < len(args); i++ {
+			vals := strings.Split(args[i], "=")
+
+			switch vals[0] {
+			case "family":
+				rule.Family = strings.Trim(vals[1], "\"")
+			case "source":
+				i++
+
+				for {
+					if args[i] == "NOT" {
+						rule.SrcAddrInvert = true
+					} else if strings.HasPrefix(args[i], "address") {
+						vals = strings.Split(args[i], "=")
+						rule.SrcAddr = strings.Trim(vals[1], "\"")
+					} else if strings.HasPrefix(args[i], "mac") {
+						// TODO
+					} else if strings.HasPrefix(args[i], "ipset") {
+						// TODO
+					}
+
+					end := false
+					for _, element := range ruleElement {
+						if strings.HasSuffix(args[i+1], element) {
+							end = true
+						}
+					}
+
+					if end {
+						break
+					} else {
+						i++
+					}
+				}
+			case "destination":
+				i++
+				if args[i] == "NOT" {
+					rule.DestAddrInvert = true
+					i++
+				}
+
+				if strings.HasPrefix(args[i], "address") {
+					vals = strings.Split(args[i], "=")
+					rule.DestAddr = strings.Trim(vals[1], "\"")
+				}
+			case "service":
+				i++
+				vals = strings.Split(args[i], "=")
+				rule.Type = ServiceRuleType
+				rule.Value = []byte(vals[1])
+			case "port":
+				vals = strings.Split(args[i+1], "=")
+				port := strings.Trim(vals[1], "\"")
+				vals = strings.Split(args[i+2], "=")
+				rule.Type = PortRuleType
+				bytes, err := json.Marshal(&Port{
+					port, strings.Trim(vals[1], "\""),
+				})
+				if err != nil {
+					return nil, err
+				}
+				rule.Value = bytes
+
+				i += 2
+			case "protocol":
+				i++
+				vals = strings.Split(args[i], "=")
+				rule.Type = ProtocolRuleType
+				rule.Value = []byte(vals[1])
+			case "masquerade":
+				rule.Type = MasqueradeRuleType
+			case "icmp-block":
+				i++
+				vals = strings.Split(args[i], "=")
+				rule.Type = IcmpBlockRuleType
+				rule.Value = []byte(vals[1])
+			case "forward-port":
+				rule.Type = ForwardPortRuleType
+				// TODO
+			case "source-port":
+				vals = strings.Split(args[i+1], "=")
+				port := strings.Trim(vals[1], "\"")
+				vals = strings.Split(args[i+2], "=")
+				rule.Type = SourcePortRuleType
+				bytes, err := json.Marshal(&Port{
+					port, strings.Trim(vals[1], "\""),
+				})
+				if err != nil {
+					return nil, err
+				}
+				rule.Value = bytes
+
+				i += 2
+			case "log":
+				rule.Log.Enabled = true
+				i++
+				if strings.HasPrefix(args[i+1], "prefix") {
+					i++
+					rule.Log.Prefix = strings.Trim(strings.Split(args[i], "=")[1], "\"")
+				}
+				if strings.HasPrefix(args[i+1], "level") {
+					i++
+					rule.Log.Level = strings.Trim(strings.Split(args[i], "=")[1], "\"")
+				}
+				if strings.HasPrefix(args[i+1], "limit") {
+					i += 2
+					rule.Log.Limit = strings.Trim(strings.Split(args[i], "=")[1], "\"")
+				}
+			case "audit":
+				rule.Audit = true
+			case "accept":
+				rule.Strategy = AcceptRuleStrategy
+			case "reject":
+				rule.Strategy = RejectRuleStrategy
+			case "drop":
+				rule.Strategy = DropRuleStrategy
+			}
+		}
+
+		rules = append(rules, rule)
+	}
+
+	return rules, nil
 }
