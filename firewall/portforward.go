8258030e68c2f9159ddf27ffcac2fead3699df17
xiayesuifeng <gdigrgg@gmail.com>
2024-07-23 14:34:43 +0800

    feat(firewall): add get port forward list function

--- firewall/portforward.go
+++ firewall/portforward.go
@@ -17,3 +17,27 @@ type PortForward struct {
 	ToPort    string `json:"toPort"`
 	ToAddress string `json:"toAddress"`
 }
+
+func GetPortForwards(zone string, permanent bool) ([]*PortForward, error) {
+	conn, err := getConn(permanent)
+	if err != nil {
+		return nil, err
+	}
+
+	forwards, err := conn.GetZoneForwardPorts(zone)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]*PortForward, 0, len(forwards))
+	for _, forward := range forwards {
+		result = append(result, &PortForward{
+			Port:      forward.Port,
+			Protocol:  ForwardProtocol(forward.Protocol),
+			ToPort:    forward.ToPort,
+			ToAddress: forward.ToAddress,
+		})
+	}
+
+	return result, nil
+}
